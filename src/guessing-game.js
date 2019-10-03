0b65c04243cec106c2771244299e96279d89386c
markmalko11 <padshik11101989@gmail.com>
2019-10-03 14:33:17 +0300

    Done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+			this.min = 0;
+			this.max = 0;
+		}
 
     setRange(min, max) {
-
+			[this.min, this.max] = [min, max]
     }
 
     guess() {
-
+			return Math.round((this.min + this.max)/2);
     }
 
     lower() {
-
+				this.max = Math.round((this.min + this.max)/2);
     }
 
     greater() {
-
+			this.min = Math.round((this.min + this.max)/2);
     }
 }
 
